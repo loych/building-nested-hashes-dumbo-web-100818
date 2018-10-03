b46db9620d8b636fa0d908aa90dd3db303ce46af
IS Choi <insil321@hotmail.com>
2018-10-03 00:51:53 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
       :hero => {:name => "Romeo", :age => "15", :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [{}]
    }, 
    :capulet => {
       :patriarch => {:name =>"Lord Capulet", :age => "50"
